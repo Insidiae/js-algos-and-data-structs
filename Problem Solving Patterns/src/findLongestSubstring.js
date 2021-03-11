528b85be66ad55762f5389f146f9aa5d05359011
Insidiae <insidiae423@gmail.com>
2021-03-11 16:50:56 +0800

    Finish Sliding Window Pattern Exercises

--- Problem Solving Patterns/src/findLongestSubstring.js
+++ Problem Solving Patterns/src/findLongestSubstring.js
@@ -1,23 +1,3 @@
-# Sliding Window - Find Longest Substring
-
-Write a function called `findLongestSubstring`, which accepts a string and returns the length of the longest substring with all distinct characters.
-
-Examples:
-* `findLongestSubstring('') // 0`
-* `findLongestSubstring('rithmschool') // 7`
-* `findLongestSubstring('thisisawesome') // 6`
-* `findLongestSubstring('thecatinthehat') // 7`
-* `findLongestSubstring('bbbbbb') // 1`
-* `findLongestSubstring('longestsubstring') // 8`
-* `findLongestSubstring('thisishowwedoit') // 6`
-
-Constraints:
-* Time Complexity - O(N)
-
----
-
-### Solution
-```js
 function findLongestSubstring(str){
   //  Create two pointers for the sliding window
   let p1 = 0;
@@ -47,4 +27,3 @@ function findLongestSubstring(str){
   //  Return the longest substring length
   return maxLen;
 }
-```
