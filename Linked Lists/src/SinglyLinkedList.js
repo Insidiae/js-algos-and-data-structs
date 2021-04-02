8edfddce44fbf18e175805f45ce62cd97321228e
Insidiae <insidiae423@gmail.com>
2021-04-03 01:10:38 +0800

    Add SLL Unshift and Shift

--- Linked Lists/src/SinglyLinkedList.js
+++ Linked Lists/src/SinglyLinkedList.js
@@ -51,4 +51,36 @@ class SinglyLinkedList {
     //  Return the value of the removed ListNode
     return value;
   }
+  //* The unshift function accepts a value to be added
+  //* to the head/start of the list
+  unshift(value) {
+    //  Create a new ListNode for the given value
+    const newNode = new ListNode(value);
+    //  Set the next property of the new node to the current head
+    //? Note that the current head should be null if the list is empty
+    newNode.next = this.head;
+    //  If list is currently empty, also set the tail
+    //  to be the new ListNode
+    if(!this.head) this.tail = newNode;
+    //  Set the new head to be the new node
+    this.head = newNode;
+    //  Increment the length property of the list
+    this.length++;
+  }
+  //* The shift function removes the item from the head/start of the list
+  //* It also returns the value of the removed item
+  shift() {
+    //  If list is empty, return undefined
+    if(!this.head) return;
+    //  Get the value on the current head of the list
+    const value = this.head.value;
+    //  Set the new head into the current head's next
+    this.head = this.head.next;
+    //  Decrement length property of the list
+    this.length--;
+    //  If list is now empty, unset the tail pointer
+    if(!this.head) this.tail = null;
+    //  Return the value of the removed ListNode
+    return value;
+  }
 }
