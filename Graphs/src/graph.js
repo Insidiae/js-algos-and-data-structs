9e093368aceadf2ca0059cda8fdf65d9bcfaeb0e
Kobe Ruado <insidiae423@gmail.com>
2021-08-06 22:46:57 +0800

    Finish Graph Traversal section

--- Graphs/src/graph.js
+++ Graphs/src/graph.js
@@ -69,4 +69,128 @@ class Graph {
     //  Optionally return the graph for easy chaining
     return this;
   }
+
+  depthFirstSearch(startVertex) {
+    //  Create an array to store results
+    const res = [];
+    //  Create an object to track visited vertices
+    const visited = {};
+
+    //  Create recursive helper function to traverse the graph
+    const traverseDepthFirst = (vertex) => {
+      //  Add vertex to results list
+      res.push(vertex);
+      //  Mark vertex as visited
+      visited[vertex] = true;
+      //  Loop over vertex's neighbors
+      for (let neighbor of this.adjacencyList[vertex]) {
+        //  If neighbor is not visited:
+        if (!visited[neighbor]) {
+          //  Make recursive call on neighbor
+          traverseDepthFirst(neighbor);
+        }
+      }
+    };
+
+    //  Invoke the recursive helper function
+    traverseDepthFirst(startVertex);
+
+    //  Return results array
+    return res;
+  }
+
+  depthFirstSearchIterative(startVertex) {
+    //  Create an array to store results
+    const res = [];
+    //  Create an object to track visited vertices
+    const visited = {};
+    //  Create a stack to store vertices to visit
+    //  and push start vertex to that stack
+    const toVisit = [startVertex];
+    //  While stack is not empty:
+    while (toVisit.length) {
+      //  Get the next vertex to visit from the stack
+      const vertex = toVisit.pop();
+      //  If vertex has not yet been visited:
+      if (!visited[vertex]) {
+        //  Add vertex to results list
+        res.push(vertex);
+        //  Mark vertex as visited
+        visited[vertex] = true;
+        //  Push all neighbors of the vertex to the stack
+        for (let neighbor of this.adjacencyList[vertex]) {
+          toVisit.push(neighbor);
+        }
+      }
+    }
+
+    //  Return results array
+    return res;
+  }
+
+  breadthFirstSearch(startVertex) {
+    //  Create an array to store results
+    const res = [];
+    //  Create an object to track visited vertices
+    const visited = {};
+    //  Create a queue to store vertices to visit
+    //  and enqueue start vertex to that queue
+    const toVisit = [startVertex];
+    //  Mark start vertex as visited
+    visited[startVertex] = true;
+
+    //  Create recursive helper function to traverse the graph
+    const traverseBreadthFirst = () => {
+      //  Get the next vertex to visit from the queue
+      const vertex = toVisit.shift();
+      //  Add vertex to results list
+      res.push(vertex);
+      //  Loop over its neighbors
+      for (let neighbor of this.adjacencyList[vertex]) {
+        //  If the neighbor is not yet visited:
+        if (!visited[neighbor]) {
+          //  Mark it as visited
+          visited[neighbor] = true;
+          //  Enqueue it to the queue
+          toVisit.push(neighbor);
+        }
+      }
+      //  If there are still vertices in the queue, keep going
+      if (toVisit.length) traverseBreadthFirst();
+    };
+
+    //  Invoke the recursive helper function
+    traverseBreadthFirst();
+    //  Return results array
+    return res;
+  }
+
+  breadthFirstSearchIterative(startVertex) {
+    //  Create an array to store results
+    const res = [];
+    //  Create an object to track visited vertices
+    const visited = {};
+    //  Create a queue to store vertices to visit
+    //  and enqueue start vertex to that queue
+    const toVisit = [startVertex];
+    //  While queue is not empty:
+    while (toVisit.length) {
+      //  Get the next vertex to visit from the queue
+      const vertex = toVisit.shift();
+      //  If vertex has not yet been visited:
+      if (!visited[vertex]) {
+        //  Add vertex to results list
+        res.push(vertex);
+        //  Mark vertex as visited
+        visited[vertex] = true;
+        //  Enqueue all neighbors of the vertex to the queue
+        for (let neighbor of this.adjacencyList[vertex]) {
+          toVisit.push(neighbor);
+        }
+      }
+    }
+
+    //  Return results array
+    return res;
+  }
 }
