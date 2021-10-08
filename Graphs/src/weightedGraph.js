09806ab1ce5a02ec8788ca9a24e3c7870fa1077d
Kobe Ruado <insidiae423@gmail.com>
2021-10-08 22:15:35 +0800

    Add notes on Bellman-Ford Algorithm

--- Graphs/src/weightedGraph.js
+++ Graphs/src/weightedGraph.js
@@ -151,4 +151,75 @@ class WeightedGraph {
     //? Make sure to reverse since we traversed from end
     return res.reverse();
   }
+
+  bellmanFord(startVertex, endVertex) {
+    //  Create an object to store the shortest
+    //  distances from the start vertex
+    const distances = {};
+
+    //  Create another object to keep track of the
+    //  previous vertices within the shortest path
+    const previousVertices = {};
+
+    for (let vertex in this.adjacencyList) {
+      //  Set each key in the distances object to be
+      //  every vertex in the adjacency list,
+      //  and set each value to be Infinity except for
+      //  the start vertex which should have a value of 0
+      distances[vertex] = vertex === startVertex ? 0 : Infinity;
+    }
+
+    //  Iterate over the graph V - 1 times
+    //? Where V = the number of vertices
+    for (let i = 0; i < Object.keys(this.adjacencyList).length - 1; i++) {
+      //  Iterate over every edge
+      for (let currentVertex in this.adjacencyList) {
+        for (let neighbor of this.adjacencyList[currentVertex]) {
+          //  Calculate the distance to the neighbor
+          //  from the starting vertex
+          const distance = distances[currentVertex] + neighbor.weight;
+          //  If the distance is less than what is
+          //  currently stored in the distances object:
+          if (distances[neighbor.vertex] > distance) {
+            //  Update the shortest distance
+            distances[neighbor.vertex] = distance;
+            //  Update the path to the neighbor stored
+            //  within the previous vertices object
+            previousVertices[neighbor.vertex] = currentVertex;
+          }
+        }
+      }
+    }
+
+    //  Iterate over every edge one more time
+    for (let currentVertex in this.adjacencyList) {
+      for (let neighbor of this.adjacencyList[currentVertex]) {
+        //  Calculate the distance to the neighbor
+        //  from the starting vertex
+        const distance = distances[currentVertex] + neighbor.weight;
+        //  If the distance is less than what is
+        //  currently stored in the distances object:
+        if (distances[neighbor.vertex] > distance) {
+          //  Indicate that a negative cycle exists
+          throw new Error("Negative cycle exists!");
+        }
+      }
+    }
+
+    //  Create an array to store results
+    const res = [];
+    //  Initialize a variable to traverse the graph,
+    //  starting from end vertex
+    let v = endVertex;
+    //  Traverse the graph using the previous vertices object
+    while (v) {
+      //  Add current vertex to results array
+      res.push(v);
+      //  Get previous vertex, if any
+      v = previousVertices[v];
+    }
+    //  Return the results array
+    //? Make sure to reverse since we traversed from end
+    return res.reverse();
+  }
 }
